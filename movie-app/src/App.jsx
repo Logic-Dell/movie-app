7a0337c2f447282e62cde953fb796b2cb6fba52f
Logic-Dell <inteldell296@gmail.com>
2025-06-02 17:35:35 -0700

    implemented useEffect to help interactivity

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked, title]);
+
   return (
-    <div className='card'>
-      <h2>{ title }</h2>
+    <div className='card' onClick={() => setCount(count + 1)}>
+      <h2>{ title } <br/> { count || null }</h2>
 
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '❤️' : '🤍'}
